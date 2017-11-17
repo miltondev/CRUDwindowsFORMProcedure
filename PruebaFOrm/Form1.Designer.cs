5b55b0fd38005e6cea5d1439f7fa71414e5186a8
miltondev <miltondev@outlook.com>
2017-11-17 06:31:24 -0600

    first commit

---
+++ PruebaFOrm/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace PruebaFOrm
+{
+    partial class MasterEmpleado
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gridEmpleado = new System.Windows.Forms.DataGridView();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboCargo = new System.Windows.Forms.ComboBox();
+            this.txtIdEmpleado = new System.Windows.Forms.TextBox();
+            this.txtNombreE = new System.Windows.Forms.TextBox();
+            this.txtApellidoE = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtBuscarEmp = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridEmpleado)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.gridEmpleado);
+            this.groupBox1.Location = new System.Drawing.Point(12, 320);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(598, 255);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Empleados";
+            // 
+            // gridEmpleado
+            // 
+            this.gridEmpleado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridEmpleado.Location = new System.Drawing.Point(13, 29);
+            this.gridEmpleado.Name = "gridEmpleado";
+            this.gridEmpleado.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridEmpleado.Size = new System.Drawing.Size(569, 209);
+            this.gridEmpleado.TabIndex = 0;
+            this.gridEmpleado.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridEmpleado_CellContentDoubleClick);
+            this.gridEmpleado.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.gridEmpleado_CellMouseDoubleClick);
+            this.gridEmpleado.DoubleClick += new System.EventHandler(this.gridEmpleado_DoubleClick);
+            this.gridEmpleado.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.gridEmpleado_KeyPress);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(6, 71);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(6, 31);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(6, 111);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Id Empleado:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre Empleado:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Apellido Empleado:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Telefono:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Cargo:";
+            // 
+            // cboCargo
+            // 
+            this.cboCargo.FormattingEnabled = true;
+            this.cboCargo.Location = new System.Drawing.Point(113, 177);
+            this.cboCargo.Name = "cboCargo";
+            this.cboCargo.Size = new System.Drawing.Size(161, 21);
+            this.cboCargo.TabIndex = 9;
+            // 
+            // txtIdEmpleado
+            // 
+            this.txtIdEmpleado.Enabled = false;
+            this.txtIdEmpleado.Location = new System.Drawing.Point(113, 33);
+            this.txtIdEmpleado.Name = "txtIdEmpleado";
+            this.txtIdEmpleado.Size = new System.Drawing.Size(68, 20);
+            this.txtIdEmpleado.TabIndex = 10;
+            // 
+            // txtNombreE
+            // 
+            this.txtNombreE.Location = new System.Drawing.Point(113, 63);
+            this.txtNombreE.Name = "txtNombreE";
+            this.txtNombreE.Size = new System.Drawing.Size(161, 20);
+            this.txtNombreE.TabIndex = 11;
+            // 
+            // txtApellidoE
+            // 
+            this.txtApellidoE.Location = new System.Drawing.Point(113, 107);
+            this.txtApellidoE.Name = "txtApellidoE";
+            this.txtApellidoE.Size = new System.Drawing.Size(161, 20);
+            this.txtApellidoE.TabIndex = 12;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(113, 136);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(161, 20);
+            this.txtTelefono.TabIndex = 13;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtIdEmpleado);
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.txtTelefono);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtApellidoE);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtNombreE);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.cboCargo);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(89, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(433, 212);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos Empleado";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnNuevo);
+            this.groupBox3.Controls.Add(this.btnGuardar);
+            this.groupBox3.Controls.Add(this.btnCancelar);
+            this.groupBox3.Location = new System.Drawing.Point(319, 33);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(90, 144);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Opciones";
+            // 
+            // txtBuscarEmp
+            // 
+            this.txtBuscarEmp.Location = new System.Drawing.Point(177, 31);
+            this.txtBuscarEmp.Name = "txtBuscarEmp";
+            this.txtBuscarEmp.Size = new System.Drawing.Size(275, 20);
+            this.txtBuscarEmp.TabIndex = 5;
+            this.txtBuscarEmp.TextChanged += new System.EventHandler(this.txtBuscarEmp_TextChanged);
+            this.txtBuscarEmp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBuscarEmp_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(74, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Apellido Empleado:";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txtBuscarEmp);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Location = new System.Drawing.Point(12, 239);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(598, 75);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Buscar Por";
+            // 
+            // MasterEmpleado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 587);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MasterEmpleado";
+            this.Text = "Master Empleado";
+            this.Load += new System.EventHandler(this.MasterEmpleado_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridEmpleado)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView gridEmpleado;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboCargo;
+        private System.Windows.Forms.TextBox txtIdEmpleado;
+        private System.Windows.Forms.TextBox txtNombreE;
+        private System.Windows.Forms.TextBox txtApellidoE;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtBuscarEmp;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox4;
+    }
+}
+
---
+++ PruebaFOrm/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Globalization;
+
+namespace PruebaFOrm
+{
+    public partial class MasterEmpleado : Form
+    {
+
+        private string strConnectionString = DB.ConfiguracionBd.Default.CadenaConexion;
+        private SqlCommand _sqlCommand;
+        private SqlDataAdapter _sqlDataAdapter;
+        DataSet _dtSet;
+
+        public MasterEmpleado()
+        {
+            InitializeComponent();
+        }
+
+
+        public void CreateConnection()
+        {
+            SqlConnection _sqlConnection = new SqlConnection(strConnectionString);
+            _sqlCommand = new SqlCommand();
+            _sqlCommand.Connection = _sqlConnection;
+        }
+        public void OpenConnection()
+        {
+            _sqlCommand.Connection.Open();
+        }
+        public void CloseConnection()
+        {
+            _sqlCommand.Connection.Close();
+        }
+        public void DisposeConnection()
+        {
+            _sqlCommand.Connection.Dispose();
+        }
+
+
+
+        private void MasterEmpleado_Load(object sender, EventArgs e)
+        {
+            LlenarComboCargoEMpleado();
+            LLenarGridEmpleado();
+        }
+
+        private void LlenarComboCargoEMpleado()
+        {
+
+            string Conexion = @"Data Source=FISICA\MSSQLSERVER2014;Initial Catalog=FormCrud;Integrated Security=True";
+            SqlConnection dataConnection = new SqlConnection(Conexion);
+            dataConnection.Open();
+            SqlCommand cmd = new SqlCommand("Sp_GridCrud", dataConnection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@Event", SqlDbType.VarChar, 10).Value = "Combobox";
+
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataConnection.Close();
+            cboCargo.ValueMember = "IdCargo";
+            cboCargo.DisplayMember = "Cargo";
+
+            cboCargo.DataSource = dt;
+        }
+
+        /*Probar para Asignar*/
+        /*
+         int index = farmRegion.FindString(myText);
+farmRegion.SelectedIndex = index;
+
+ */
+        private void LLenarGridEmpleado()
+        {
+
+            string Conexion = @"Data Source=FISICA\MSSQLSERVER2014;Initial Catalog=FormCrud;Integrated Security=True";
+            SqlConnection dataConnection = new SqlConnection(Conexion);
+            try
+            {
+
+
+                dataConnection.Open();
+                SqlCommand cmd = new SqlCommand("Sp_GridCrud", dataConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@Event", SqlDbType.VarChar, 10).Value = "Select";
+
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataConnection.Close();
+                gridEmpleado.DataSource = dt;
+
+                /*
+
+                SqlDataReader reader;
+
+
+
+                CreateConnection();
+                OpenConnection();
+                _sqlCommand.CommandText = "Sp_GridCrud";
+                _sqlCommand.CommandType = CommandType.StoredProcedure;
+                _sqlCommand.Parameters.AddWithValue("@Event", "Select");
+                reader = _sqlCommand.ExecuteReader();
+                _sqlDataAdapter = new SqlDataAdapter(_sqlCommand);
+                _dtSet = new DataSet();
+                _sqlDataAdapter.Fill(_dtSet);
+                gridEmpleado.DataSource = _dtSet;*/
+                //    gridEmpleado.DataBind();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("The Error is " + ex);
+            }
+            finally
+            {
+                dataConnection.Close();
+                dataConnection.Dispose();
+            }
+        }
+
+        private void Guardar()
+        {
+
+        }
+
+        private void Cancelar()
+        {
+
+        }
+
+        private void NuevoEmpleado()
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+            string Conexion = @"Data Source=FISICA\MSSQLSERVER2014;Initial Catalog=FormCrud;Integrated Security=True";
+            SqlConnection dataConnection = new SqlConnection(Conexion);
+
+            int idcargo = cboCargo.SelectedIndex;
+
+
+
+
+
+            dataConnection.Open();
+            SqlCommand cmd = new SqlCommand("Sp_GridCrud", dataConnection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Event", "Add");
+            cmd.Parameters.AddWithValue("@FirstName", Convert.ToString(txtNombreE.Text.Trim()));
+            cmd.Parameters.AddWithValue("@LastName", Convert.ToString(txtApellidoE.Text.Trim()));
+            cmd.Parameters.AddWithValue("@PhoneNumber", Convert.ToString(txtTelefono.Text.Trim()));
+            cmd.Parameters.AddWithValue("@IdCargo", Convert.ToInt32(idcargo));
+            int result = Convert.ToInt32(cmd.ExecuteNonQuery());
+            if (result > 0)
+            {
+                MessageBox.Show("Se ha Guardado Correctamente");
+            }
+            else
+            {
+
+                MessageBox.Show("Ha Ocurrido un Error");
+            }
+
+            dataConnection.Close();
+        }
+
+        private void gridEmpleado_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void txtBuscarEmp_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+
+
+        }
+
+        private void txtBuscarEmp_TextChanged(object sender, EventArgs e)
+        {
+            (gridEmpleado.DataSource as DataTable).DefaultView.RowFilter = string.Format("ApellidoE LIKE '%{0}%'", txtBuscarEmp.Text);
+        }
+
+        private void gridEmpleado_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+        }
+
+        private void gridEmpleado_DoubleClick(object sender, EventArgs e)
+        {
+
+
+
+     
+            /*
+
+
+
+                string Cargo = "";
+            if (gridEmpleado.SelectedRows.Count < 1)
+            {
+                MessageBox.Show("Please select a client");
+                return;
+            }
+
+            else
+            {
+                int ro = gridEmpleado.SelectedIndex;
+                txtIdEmpleado.Text = gridEmpleado.SelectedRows[0].Cells["IdEmpleado"].Value.ToString();
+                txtNombreE.Text = gridEmpleado.SelectedRows[0].Cells["NombreE"].Value.ToString();
+                txtApellidoE.Text = gridEmpleado.SelectedRows[0].Cells["ApellidoE"].Value.ToString();
+                txtTelefono.Text = gridEmpleado.SelectedRows[0].Cells["Telefono"].Value.ToString();
+                Cargo = gridEmpleado.SelectedRows[0].Cells["Cargo"].Value.ToString();
+
+                int index = cboCargo.FindString(Cargo);
+                cboCargo.SelectedIndex = index;
+            }*/
+        }
+
+        private void gridEmpleado_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            string Cargo = "";
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = this.gridEmpleado.Rows[e.RowIndex];
+                //Eid_txt.Text = row.Cells["Employee ID"].Value.ToString();
+                txtIdEmpleado.Text = row.Cells["IdEmpleado"].Value.ToString();
+                txtNombreE.Text = row.Cells["NombreE"].Value.ToString();
+                txtApellidoE.Text = row.Cells["ApellidoE"].Value.ToString();
+                txtTelefono.Text = row.Cells["Telefono"].Value.ToString();
+                Cargo = row.Cells["Cargo"].Value.ToString();
+
+                int index = cboCargo.FindString(Cargo);
+                cboCargo.SelectedIndex = index;
+
+            }
+        }
+
+        private void Update ()
+        {
+            string UpdateCommand = "sptimeupdate";
+            using (SqlConnection sqlConnectionCmdString = new SqlConnection(......))
+            using (SqlCommand sqlRenameCommand = new SqlCommand(UpdateCommand, sqlConnectionCmdString))
+            {
+                DateTime td = Convert.ToDateTime(toolDate.Text);
+                sqlRenameCommand.CommandType = CommandType.StoredProcedure;
+                sqlRenameCommand.Parameters.Add("@id", SqlDbType.NVarChar).Value = Clientid[up].ToString();
+                sqlRenameCommand.Parameters.Add("@date", SqlDbType.DateTime).Value = td;
+                sqlConnectionCmdString.Open();
+                sqlRenameCommand.ExecuteNonQuery();
+            }
+        }
+
+
+
+    }
+}
